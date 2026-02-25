eb7fe0bb64e67503f234dba800e4ce90616c7fb6
Simon Stroomer <ss7442@sd62.bc.ca>
2026-02-25 15:20:52 -0800

    figure something new out. Can't really wrap my head around this.

--- Alkali/core/include/json/JsonDocument.h
+++ Alkali/core/include/json/JsonDocument.h
@@ -3,6 +3,11 @@
 
 #include "system/String.h"
 
+#include "math/Vector2.h"
+#include "math/Vector3.h"
+#include "math/Matrix.h"
+#include "math/Quaternion.h"
+
 #include <list>
 
 namespace alk {
@@ -17,6 +22,16 @@ namespace alk {
 		void RemoveNode(const tString& asName);
 		
 		cJsonNode* GetJsonNode(const tString& asName);
+
+		tString GetValueString(const tString& asName, int alIdx = 0);
+		int GetValueInt(const tString& asName, size_t alIdx = 0);
+		float GetValueFloat(const tString& asName, size_t alIdx = 0);
+		tVector2l GetValueVector2l(const tString& asName, size_t alIdx = 0);
+		tVector2f GetValueVector2f(const tString& asName, size_t alIdx = 0);
+		tVector3l GetValueVector3l(const tString& asName, size_t alIdx = 0);
+		tVector3f GetValueVector3f(const tString& asName, size_t alIdx = 0);
+		tMatrixf GetValueMatrixf(const tString& asName, size_t alIdx = 0);
+		cQuaternion GetValueQuaternion(const tString& asName, size_t alIdx = 0);
 
 	private:
 		tString msName;
