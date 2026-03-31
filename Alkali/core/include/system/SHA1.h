efce30998733f0b37b0fd9293773cd0127c7d356
Simon Stroomer <simon.stroomer@gmail.com>
2026-03-31 06:58:20 -0700

    start working on SHA-1

--- Alkali/core/include/system/SHA1.h
+++ Alkali/core/include/system/SHA1.h
@@ -11,19 +11,7 @@ namespace alk {
 		cSHA1();
 		~cSHA1();
 
-		void Reset();
 
-		void input();
-
-		void Result();
-
-		unsigned int PadMessage();
-
-		unsigned int ProcessMessage();
-
-		//functions
-		unsigned int ROTL(int N, unsigned int W);
-		unsigned int ROTR(int N, unsigned int W);
 
 	private:
 		unsigned int H[5]; // initial hashing values
