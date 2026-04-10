f84323c2f047466e668e9aca382b100eb31ba0cc
Simon Stroomer <ss7442@sd62.bc.ca>
2026-04-10 14:24:47 -0700

    Donkey kong i not a fonkry????

--- Alkali/core/include/math/Matrix.h
+++ Alkali/core/include/math/Matrix.h
@@ -93,6 +93,17 @@ namespace alk {
 			return buf;
 		}
 
+		inline tString ToFileString() const
+		{
+			char buf[512];
+			sprintf(buf, "%g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g",
+				m[0][0], m[0][1], m[0][2], m[0][3],
+				m[1][0], m[1][1], m[1][2], m[1][3],
+				m[2][0], m[2][1], m[2][2], m[2][3],
+				m[3][0], m[3][1], m[3][2], m[3][3]);
+			return buf;
+		}
+
 		static const cMatrix<T> identity;
 		static const cMatrix<T> zero;
 		static const cMatrix<T> sign;
