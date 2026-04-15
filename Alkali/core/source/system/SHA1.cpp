112fa9f6c770309e354363679eca7fd8e5c5addb
Simon Stroomer <ss7442@sd62.bc.ca>
2026-04-15 14:32:20 -0700

    finished commenting SHA-1 code, godspeed for the texture ones

--- Alkali/core/source/system/SHA1.cpp
+++ Alkali/core/source/system/SHA1.cpp
@@ -7,13 +7,14 @@ namespace alk {
 
 	cSHA1::cSHA1()
 	{
-		Reset();
+		Reset(); //reset values to initial
 	}
 
 	//----------------------------------------------------------
 
 	cSHA1::~cSHA1()
 	{
+		//does nothing
 	}
 
 	//----------------------------------------------------------
@@ -35,49 +36,49 @@ namespace alk {
 
 	tString cSHA1::Hash(const tString& asString)
 	{
-		Input((const unsigned char*)asString.c_str(), asString.size());
+		Input((const unsigned char*)asString.c_str(), asString.size()); // take the size and data of a std::string for input
 
-		PadMessage();
+		PadMessage(); // pad message until a multiple of 512 bits is reached
 
-		ProcessMessage();
+		ProcessMessage(); // hash the message
 		
-		return Format();
+		return Format(); // take the hashes, add them together and output the resulting string
 	}
 
 	//----------------------------------------------------------
 
 	twString cSHA1::Hash(const twString& asString)
 	{
-		Input((const unsigned char*)cString::To8BitChar(asString).c_str(), cString::To8BitChar(asString).size());
+		Input((const unsigned char*)cString::To8BitChar(asString).c_str(), cString::To8BitChar(asString).size());// convert the std::wstring to std::string, can introduce some issues with Unicode charaters, but it'll do for now.
 
-		PadMessage();
+		PadMessage();// pad message until a multiple of 512 bits is reached
 
-		ProcessMessage();
+		ProcessMessage();// hash the message
 
-		return cString::ToWideChar(Format());
+		return cString::ToWideChar(Format());// take the hashes, add them together, convert the std::string to a std::wstring, and output the resulting string
 	}
 
 	//----------------------------------------------------------
 
 	void cSHA1::Input(const unsigned char* asMessage, unsigned int alLength)
 	{
-		while (alLength--)
+		while (alLength--) // for every character of the input word
 		{
 			
-			mlLength+=8;
+			mlLength+=8; // increment our length in bits, used for padding the last bytes of the message before we hash.
 			if (mlLength == 0)
 			{
-				// do nothing for now
+				// do nothing for now, if it's 0 there been an overflow, which means the length of the message has exceeded 2^32, since I've elected to ignore messages that size, it won't matter
 			}
 
-			mlMessageBlock[mlMessageBlockIndex++] = (*asMessage );
+			mlMessageBlock[mlMessageBlockIndex++] = (*asMessage ); //message block at the index is equal to the specific character of the original string
 
-			if (mlMessageBlockIndex == 64)
+			if (mlMessageBlockIndex == 64) // if the message is perfectly filled up before we even need to compute it
 			{
-				ProcessMessage();
+				ProcessMessage(); // hash it
 			}
 
-			asMessage++;
+			asMessage++; // get the next char
 		}
 	}
 
@@ -86,44 +87,44 @@ namespace alk {
 	void cSHA1::PadMessage()
 	{
 		/*
-		* If the message is too long, we'll need to pad into a
+		* If the message is too long, we'll need to pad some amount of bits into a
 		* seperate message block
 		*/
 		if (mlMessageBlockIndex > 55)
 		{
-			mlMessageBlock[mlMessageBlockIndex++] = 0x80;
+			mlMessageBlock[mlMessageBlockIndex++] = 0x80; // set the next bit equal to 1 in ASCII
 
 			while (mlMessageBlockIndex < 64)
 			{
-				mlMessageBlock[mlMessageBlockIndex++] = 0x0;
+				mlMessageBlock[mlMessageBlockIndex++] = 0x0; // appened 0s until we wreach 512 bits
 			}
 
-			ProcessMessage();
+			ProcessMessage(); // since the message is too long, we need to hash it now
 
-			while (mlMessageBlockIndex < 56)
+			while (mlMessageBlockIndex < 56) // loop is reset to 0 in PadMessage(), so pad with 0s again until we can store length in the last 8 bytes. Note though that this isn't technically good, since this can't properly process messages 2^32 in length, but whatever.
 			{
-				mlMessageBlock[mlMessageBlockIndex++] = 0x0;
+				mlMessageBlock[mlMessageBlockIndex++] = 0x0; // appened 0s until we wreach 512 bits
 			}
 		}
-		else //otherwise just pad until we reach 448 bits 
+		else // <512 bit message size, so just pad until 512 bits are present
 		{
-			mlMessageBlock[mlMessageBlockIndex++] = 0x80;
+			mlMessageBlock[mlMessageBlockIndex++] = 0x80;// set the next bit equal to 1 in ASCII
 
-			while (mlMessageBlockIndex < 56)
+			while (mlMessageBlockIndex < 56) 
 			{
-				mlMessageBlock[mlMessageBlockIndex++] = 0x0;
+				mlMessageBlock[mlMessageBlockIndex++] = 0x0;// appened 0s until we wreach 512 bits
 			}
 		}
 
-		// store the length in the last 8 bytes.
-		mlMessageBlock[56] = 0;
-		mlMessageBlock[57] = 0;
-		mlMessageBlock[58] = 0;
-		mlMessageBlock[59] = 0;
-		mlMessageBlock[60] = (mlLength >> 24) & 0xFF;
-		mlMessageBlock[61] = (mlLength >> 16) & 0xFF;
-		mlMessageBlock[62] = (mlLength >> 8) & 0xFF;
-		mlMessageBlock[63] = (mlLength) & 0xFF;
+		// store the length in the last 8 bytes. Should technically have more going here but if the message exceeds 2^32 bits in length there comes a point where you just take it on the chin and be wrong
+		mlMessageBlock[56] = 0;//appened 0
+		mlMessageBlock[57] = 0;//appened 0
+		mlMessageBlock[58] = 0;//appened 0
+		mlMessageBlock[59] = 0;//appened 0
+		mlMessageBlock[60] = (mlLength >> 24) & 0xFF; //appened first 2 bytes of length 
+		mlMessageBlock[61] = (mlLength >> 16) & 0xFF;//appened middle 2 bytes of length 
+		mlMessageBlock[62] = (mlLength >> 8) & 0xFF;//appened next set of middle 2 bytes of length 
+		mlMessageBlock[63] = (mlLength) & 0xFF;//appened last 2 bytes of length
 	}
 
 	//----------------------------------------------------------
@@ -136,7 +137,7 @@ namespace alk {
 		unsigned int W[80]; // 512-bit message schedule
 		unsigned int ltemp; // temporary buffer. As specified by FIPS 180-4
 
-		// setup constants
+		// setup constants. These are defined in SHS 180-4
 		const unsigned int K[] = {
 			0x5a827999,
 			0x6ed9eba1,
@@ -147,27 +148,36 @@ namespace alk {
 		// initialize the message schedule
 		for (t=0; t < 16; t++)
 		{
-			W[t] = ((unsigned int)mlMessageBlock[t * 4]) << 24;
-			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 1]) << 16;
-			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 2]) << 8;
-			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 3]);
+			W[t] = ((unsigned int)mlMessageBlock[t * 4]) << 24; // add first byte to the schedule
+			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 1]) << 16;// add second byte to the schedule
+			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 2]) << 8;// add third byte to the schedule
+			W[t] |= ((unsigned int)mlMessageBlock[t * 4 + 3]);// add fourth byte to the schedule
 		}
 
 		for (t=16; t < 80; t++)
 		{
-			W[t] = ROTL((W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16]), 1);
+			W[t] = ROTL((W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16]), 1); // just do this I guess. FIPS demands it to initialize the last 56 bytes of the schedule. I'm not a cryptography expert, but I reckon it makes it more secure. 
 		}
 
 		// initialize working variables
-		A = H[0];
+		A = H[0]; 
 		B = H[1];
 		C = H[2];
 		D = H[3];
 		E = H[4];
 
-		// looping through hashing functions
-		// function names are provided as they are named in FIPS 180-4
-		for (t = 0; t < 20; t++) // choose
+		/*
+			 looping through hashing functions
+			function names are provided as they are named in FIPS 180-4
+
+			Fair warning: Understanding the *why* behind these function is way above my pay-grade (and probably my security clearance level).
+			If the following seems logically confusing -yeah me too.
+
+			Interesting fact:
+			Most of the algorithm is identical between different loops, save for which constant K is being used, and the function ltemp is being set to first.
+		*/
+		
+		for (t = 0; t < 20; t++) // Choose function
 		{
 			ltemp = ROTL(A, 5) + ((B & C) ^ ((~B) & D)) + E + K[0] + W[t];
 			E = D;
@@ -177,7 +187,7 @@ namespace alk {
 			A = ltemp;
 		}
 
-		for (t=20; t < 40; t++) // parity
+		for (t=20; t < 40; t++) // Parity function
 		{
 			ltemp = ROTL(A, 5) + (B ^ C ^ D) + E + K[1] + W[t];
 			E = D;
@@ -187,7 +197,7 @@ namespace alk {
 			A = ltemp;
 		}
 
-		for (t = 40; t < 60; t++) // majority
+		for (t = 40; t < 60; t++) // Majority function
 		{
 			ltemp = ROTL(A, 5) + ((B & C) ^ (B & D) ^ (C & D)) + E + K[2] + W[t];
 			E = D;
@@ -197,7 +207,7 @@ namespace alk {
 			A = ltemp;
 		}
 		
-		for (t = 60; t < 80; t++) // parity
+		for (t = 60; t < 80; t++) // Parity function, again
 		{
 			ltemp = ROTL(A, 5) + (B ^ C ^ D) + E + K[3] + W[t];
 			E = D;
@@ -214,7 +224,7 @@ namespace alk {
 		H[3] = D + H[3];
 		H[4] = E + H[4];
 
-		mlMessageBlockIndex = 0;
+		mlMessageBlockIndex = 0; //reset the index for repadding long messages
 	}
 
 	//----------------------------------------------------------
